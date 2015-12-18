39f69d412e7bc02df01599acc6e3ce2f302f4c4f
carlhoward <orders@carlhoward.org>
2015-12-17 23:49:12 -0700

    Implement lifo and fifo queues in place of slices.

--- ballclock.go
+++ ballclock.go
@@ -1,3 +1,22 @@
+
+//
+// Program to simulate operation of a mechanical ball clock
+//
+// This program is an exercise in learning the go programming language.
+// 
+// The clock simulation executes in 2 modes of operation determined by
+// the number of arguments given:
+//
+// MODE_1 usage: ballclock numberBalls
+//     -> run until state repeats, then display days elapsed
+//
+// MODE_2 usage: ballclock numberBalls targetMinutes
+//     -> run for given time in minutes, then display state
+//
+// representational tracks are implemented as lifo queues while the overflow
+// track is implemented as a ring buffer (fifo queue).
+//
+
 package main
 
 import (
@@ -7,142 +26,121 @@ import (
     "encoding/json"
 )
 
-type track struct {
-    name string
-    balls []int
-    limit int
-    nextTrack *track
-    mainTrack *track
-    cachedState []int
+var numberBalls int
+
+var overflow ring // ring buffer as overflow track
+var min track // minute track
+var fmin track // five-minute track
+var hour track // hour track 	
+
+type buffer interface {
+    push(int)
+    pop() int
+    val() []int
 }
 
-func (t *track) count() int {
-    return len(t.balls)
+//  ring buffer definitions (fifo queue)
+
+type ring struct {
+    balls [128]int
+    start int
+    end int
 }
 
-func (t *track) val() string {
-    return fmt.Sprintf("%s %v\n", t.name, t.balls)
+func (r *ring) push(b int) {
+    r.balls[r.end] = b
+    r.end = (r.end + 1) & 0x7F
 }
 
-func (t *track) cacheState() {
-    t.cachedState = make([]int, len(t.balls), len(t.balls))
-    copy(t.cachedState, t.balls)
-}
-
-func (t *track) sameState() bool {
-    if len(t.balls) != len(t.cachedState) {
-        return false
-    }
-    for i := 0; i < len(t.balls); i++ {
-        if t.balls[i] != t.cachedState[i] {
-            return false
-        }
-    }
-    return true
-}
-
-func (t *track) remFirstBall() int {
-    l := len(t.balls)
-    if (l == 0) {
-        fmt.Println("Error: empty overflow track")
-        os.Exit(1)
-    }
-    b := t.balls[0]
-    t.balls = t.balls[1:]
+func (r *ring) pop() int {
+    b := r.balls[r.start]
+    r.start = (r.start + 1) & 0x7F
     return b
 }
 
-func (t *track) remLastBall() int {
-    l := len(t.balls)
-    b := t.balls[l - 1]
-    t.balls = t.balls[:l-1]
-    return b
-}
-
-func (t *track) clear() {
-    t.balls = t.balls[:0]
-}
-
-func (t *track) addBall(b int) {
-//    fmt.Printf("Adding %d to %v\n", b, t)
-    if (t.limit != 0 && len(t.balls) == t.limit) {
-        // cascade
-        for (t.count() > 0) {
-            t.mainTrack.addBall(t.remLastBall())
-        }
-        t.nextTrack.addBall(b)
+func (r *ring) val() []int {
+    if (r.start < r.end) {
+        return overflow.balls[r.start : r.end]
     } else {
-        t.balls = append(t.balls, b)
+        return append(r.balls[r.start : 128], r.balls[0 : r.end]...)
     }
 }
 
-func advance() {
-    minTrack.addBall(mainTrack.remFirstBall())
-}
-
-func getTime() string {
-    mins := minTrack.count() + 5 * fiveminTrack.count()
-    hours := hourTrack.count() + 1
-    s := "time: " + strconv.Itoa(hours) + ":"
-    if (mins < 10) {
-        s += "0"
-    }
-    return s + strconv.Itoa(mins)
-}
-
-func cacheState() {
-    for _, track := range tracks {
-        track.cacheState()
-    }
-}
-
-func sameState() bool {
-    for _, track := range tracks {
-        if !track.sameState() {
-            return false
+// return true if ring buffer is in original state
+func (r *ring) check() bool {
+    val := 1;
+    if r.start < r.end {
+        for i := r.start; i < r.end; i++ {
+            if r.balls[i] != val {
+                return false
+            }
+            val++
+        }
+    } else {
+        for i := r.start; i < 128; i++ {
+            if r.balls[i] != val {
+                return false
+            }
+            val++
+        }
+        for i := 0; i < r.end; i++ {
+            if r.balls[i] != val {
+                return false
+            }
+            val++
         }
     }
     return true
 }
 
-func getState() map[string][]int {
-    m := make(map[string][]int)
-    for _, track := range tracks {
-        m[track.name] = track.balls
-    }
-    return m
+//  track definitions (lifo queue)
+
+type track struct {
+    balls [12]int
+    count int
+    limit int
+    next buffer
 }
 
-func initTracks(n int) {
-    clear()
-    // add balls to the overflow track
-    // this gives starting time of 1:00
-    for b := 1; b <= n; b++ {
-        mainTrack.addBall(b)
+func (t *track) push(b int) {
+    if (t.count == t.limit) {
+        // empty track into overflow and cascade to next track
+        for i := 0; i < t.limit; i++ {
+            overflow.push(t.pop())
+        }
+        t.next.push(b)
+    } else {
+        t.balls[t.count] = b
+        t.count++
     }
 }
 
-func clear() {
-    elapsedMinutes = 0
-    for _, track := range tracks {
-        track.clear()
-    }
+func (t *track) pop() int {
+    t.count--
+    return t.balls[t.count]
+}
+
+func (t *track) val() []int {
+    return t.balls[0:t.count]
+}
+
+// main functions
+
+type state struct {
+    Min []int
+    FiveMin []int
+    Hour []int
+    Main []int
+}
+
+func getState() state {
+    return state{Min: min.val(), FiveMin: fmin.val(), Hour: hour.val(), Main: overflow.val()}
 }
 
 func exitUsage() {
         fmt.Printf("Usage: %s ballCount [timeToRun]\n", os.Args[0])
         os.Exit(0)
 }
-
-var minTrack track
-var fiveminTrack track
-var hourTrack track
-var mainTrack track
-var tracks []*track
-
-var numberBalls int
-var targetMinutes int
-var elapsedMinutes int
 
 func getIntegerArg(argIndex int) int {
     intVal, err := strconv.Atoi(os.Args[argIndex])
@@ -153,15 +151,15 @@ func getIntegerArg(argIndex int) int {
 }
 
 func main() {
-
+    targetMins := 0
+    halfDays := 0
     if len(os.Args) == 2 {
         // MODE_1: run until state repeats, then display days elapsed
         numberBalls = getIntegerArg(1)
-        targetMinutes = 0
     } else if len(os.Args) == 3 {
         // MODE_2: run for given time in minutes, then display state
         numberBalls = getIntegerArg(1)
-        targetMinutes = getIntegerArg(2)
+        targetMins = getIntegerArg(2)
     } else {
         exitUsage()
     }
@@ -170,41 +168,44 @@ func main() {
         exitUsage()
     }
 
-    // create all tracks
-    minTrack = track{name: "Min", limit: 4}
-    fiveminTrack = track{name: "FiveMin", limit: 11}
-    hourTrack = track{name: "Hour", limit: 11}
-    mainTrack = track{name: "Main", limit: 0}
-    tracks = []*track{&minTrack, &fiveminTrack, &hourTrack, &mainTrack}
+    // create tracks
+    min = track{count: 0, limit: 4}
+    fmin = track{count: 0, limit: 11}
+    hour = track{count: 0, limit: 11}
+    overflow = ring{start: 0, end: 0}
 
     // link tracks
-    minTrack.nextTrack = &fiveminTrack
-    fiveminTrack.nextTrack = &hourTrack
-    hourTrack.nextTrack = &mainTrack
+    min.next = &fmin
+    fmin.next = &hour
+    hour.next = &overflow
 
-    // assign the overflow track
-    for _, track := range tracks {
-        track.mainTrack = &mainTrack
+    // initialize balls [1 .. n]
+    for b := 1; b <= numberBalls; b++ {
+        overflow.push(b)
     }
-
-    initTracks(numberBalls)
-    cacheState()
-
-    // main loop
-    for {
-        advance()
-        elapsedMinutes++
-        if targetMinutes != 0 {
-            if elapsedMinutes == targetMinutes {
-                strJ, _ := json.Marshal(getState())
-                fmt.Printf("State for %d balls after %d minutes:\n%s\n", numberBalls, elapsedMinutes, strJ)
+    
+    // pick run loop based on mode of operation
+    if targetMins == 0 {
+        for {
+            // loop for 12 hours (= 720 minutes)
+            for i := 0;  i < 720; i++ {
+                b := overflow.pop()
+                min.push(b)
+            }
+            halfDays++
+            if overflow.check() {
+                days := halfDays / 2
+                fmt.Printf("%d balls cycle after %d days.\n", numberBalls, days)
                 break
             }
-        } else if(sameState()) {
-            days := elapsedMinutes / 60 / 24
-            fmt.Printf("%d balls cycle after %d days.\n", numberBalls, days)
-            break
         }
+    } else {
+        // loop for allotted time
+        for i := 0;  i < targetMins; i++ {
+            min.push(overflow.pop())
+        }
+        strJ, _ := json.Marshal(getState())
+        fmt.Printf("%s\n", string(strJ))
     }
 
 }
